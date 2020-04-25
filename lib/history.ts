6f68faef29c0036e354f2216891771fdc4b3e4e7
David Vázquez Púa <davazp@gmail.com>
2020-04-25 23:10:09 +0200

    Implement History outside of a React hook

--- lib/history.ts
+++ lib/history.ts
@@ -11,10 +11,75 @@ interface HistoryPast<A> {
   alternativeFutures: Array<HistoryFuture<A>>;
 }
 
-interface HistoryPresent<A> {
-  past: HistoryPast<A> | null;
+class HistoryPresent<A> {
   state: A;
-  futures: Array<HistoryFuture<A>>;
+  private past: HistoryPast<A> | null;
+  private futures: Array<HistoryFuture<A>>;
+
+  constructor({
+    state,
+    past,
+    futures,
+  }: {
+    state: A;
+    past: HistoryPast<A> | null;
+    futures: Array<HistoryFuture<A>>;
+  }) {
+    this.state = state;
+    this.past = past;
+    this.futures = futures;
+  }
+
+  update(newState: A) {
+    return new HistoryPresent({
+      past: {
+        state: this.state,
+        past: this.past,
+        alternativeFutures: this.futures,
+      },
+      state: newState,
+      futures: [],
+    });
+  }
+
+  undo() {
+    if (this.past) {
+      return new HistoryPresent({
+        past: this.past.past,
+        state: this.past.state,
+        futures: [
+          {
+            state: this.state,
+            futures: this.futures,
+          },
+          ...this.past.alternativeFutures,
+        ],
+      });
+    } else {
+      return this;
+    }
+  }
+
+  redo() {
+    if (this.futures.length > 0) {
+      const [future, ...alternativeFutures] = this.futures;
+      return new HistoryPresent({
+        past: {
+          past: this.past,
+          state: this.state,
+          alternativeFutures,
+        },
+        state: future.state,
+        futures: future.futures,
+      });
+    } else {
+      return this;
+    }
+  }
+
+  static from<A>(initialValue: A): HistoryPresent<A> {
+    return new HistoryPresent({ state: initialValue, past: null, futures: [] });
+  }
 }
 
 export interface HistoryState {
@@ -25,49 +90,22 @@ export interface HistoryState {
 export function useStateWithHistory<A>(
   initialState: A
 ): [A, (newState: A) => void, HistoryState] {
-  const [present, setPresent] = useState<HistoryPresent<A>>({
-    past: null,
-    state: initialState,
-    futures: [],
-  });
+  const [present, setPresent] = useState<HistoryPresent<A>>(() =>
+    HistoryPresent.from(initialState)
+  );
+
   const state = present.state;
 
   const setState = (newState: A) => {
-    setPresent({
-      past: {
-        state: present.state,
-        past: present.past,
-        alternativeFutures: present.futures,
-      },
-      state: newState,
-      futures: [],
-    });
+    setPresent(present.update(newState));
   };
 
   const history: HistoryState = {
     undo() {
-      if (present.past) {
-        setPresent({
-          past: present.past.past,
-          state: present.past.state,
-          futures: [present, ...present.past.alternativeFutures],
-        });
-      }
+      setPresent(present.undo());
     },
-
     redo() {
-      if (present.futures.length > 0) {
-        const [future, ...alternativeFutures] = present.futures;
-        setPresent({
-          past: {
-            past: present.past,
-            state: present.state,
-            alternativeFutures,
-          },
-          state: future.state,
-          futures: future.futures,
-        });
-      }
+      setPresent(present.redo());
     },
   };
 
